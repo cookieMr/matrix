e24a1ddc84696d84a250b936188a88c389d051ea
Dave <dawid.boisse@gmail.com>
2020-07-06 10:17:52 +0200

    Implement addition of two matrices
    Closes #5
    Closes #6

---
+++ src/main/java/mr/cookie/matrix/utils/MatrixMath.java
@@ -0,0 +1,50 @@
+package mr.cookie.matrix.utils;
+
+import mr.cookie.matrix.model.Matrix;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public final class MatrixMath {
+
+    private MatrixMath() {
+        throw new AssertionError("MatrixMath class should not be instantiated.");
+    }
+
+    @NotNull
+    public static Matrix<Integer> add(@NotNull Matrix<Integer> m1, @NotNull Matrix<Integer> m2) {
+        verifyRowCount(m1, m2);
+        verifyColumnCount(m1, m2);
+
+        final int rowSize = m1.getRowSize();
+        final int columnSize = m1.getColumnSize();
+
+        Integer[] numbers = new Integer[rowSize * columnSize];
+        for (int c = 0; c < columnSize; c++) {
+            List<Integer> rowM1 = m1.getRow(c);
+            List<Integer> rowM2 = m2.getRow(c);
+            for (int r = 0; r < rowSize; r++) {
+                numbers[rowSize * c + r] = rowM1.get(r) + rowM2.get(r);
+            }
+        }
+
+        return new Matrix<>(rowSize, columnSize, numbers);
+    }
+
+    private static void verifyRowCount(Matrix<Integer> m1, Matrix<Integer> m2) {
+        if (m1.getRowSize() != m2.getRowSize()) {
+            throw new IllegalArgumentException(
+                    String.format("Both matrices must have the same row count. Provided sizes are [%d] and [%d]",
+                            m1.getRowSize(), m2.getRowSize()));
+        }
+    }
+
+    private static void verifyColumnCount(Matrix<Integer> m1, Matrix<Integer> m2) {
+        if (m1.getColumnSize() != m2.getColumnSize()) {
+            throw new IllegalArgumentException(
+                    String.format("Both matrices must have the same column count. Provided sizes are [%d] and [%d]",
+                            m1.getColumnSize(), m2.getColumnSize()));
+        }
+    }
+
+}
---
+++ src/test/java/mr/cookie/matrix/utils/MatrixMathTest.java
@@ -0,0 +1,49 @@
+package mr.cookie.matrix.utils;
+
+import mr.cookie.matrix.model.Matrix;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MatrixMathTest {
+
+    @Test
+    void invalidRowSizesThrowException() {
+        Matrix<Integer> m1 = Generator.random(1, 2);
+        Matrix<Integer> m2 = Generator.random(2, 2);
+
+        assertThatThrownBy(() -> MatrixMath.add(m1, m2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Both matrices must have the same row count. Provided sizes are [%d] and [%d]", 1, 2);
+    }
+
+    @Test
+    void invalidColumnSizesThrowException() {
+        Matrix<Integer> m1 = Generator.random(2, 1);
+        Matrix<Integer> m2 = Generator.random(2, 2);
+
+        assertThatThrownBy(() -> MatrixMath.add(m1, m2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Both matrices must have the same column count. Provided sizes are [%d] and [%d]", 1, 2);
+    }
+
+    @Test
+    void add() {
+        Matrix<Integer> matrix1 = new Matrix<>(2, 2, 1, 2, 3, 4);
+        Matrix<Integer> matrix2 = new Matrix<>(2, 2, 4, 3, 2, 1);
+        Matrix<Integer> result = MatrixMath.add(matrix1, matrix2);
+
+        Assertions.assertAll(
+                () -> assertThat(result.getRowSize()).isEqualTo(matrix1.getRowSize()),
+                () -> assertThat(result.getColumnSize()).isEqualTo(matrix1.getColumnSize()),
+                () -> assertThat(result.get(0, 0)).isEqualTo(5),
+                () -> assertThat(result.get(1, 1)).isEqualTo(5)
+
+                // TODO: lombok and equals/hashcode
+                // () -> assertThat(result).isEqualTo(new Matrix<>(2, 2, 5, 5, 5, 5))
+        );
+    }
+
+}
