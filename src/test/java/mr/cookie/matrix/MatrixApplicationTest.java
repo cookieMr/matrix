01ba9bc4410019112c1ec295caefe9479c663018
cookieMr <dawid.boisse@gmail.com>
2020-07-10 21:32:06 +0200

    Improve test coverage

---
+++ src/test/java/mr/cookie/matrix/MatrixApplicationTest.java
@@ -0,0 +1,14 @@
+package mr.cookie.matrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixApplicationTest {
+
+    @Test
+    public void contextLoads() {
+        // checks if spring boot runs with no errors
+    }
+
+}
